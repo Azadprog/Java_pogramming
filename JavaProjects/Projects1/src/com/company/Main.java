e9654792e89e39ad55e55cdb02180d3b8dac68bc
Azadprog <rehman19942008@hotmail.com>
2019-04-02 12:41:06 +0200

    adding another Directory

---
+++ JavaProjects/Projects1/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("This is a hack ");
+
+        System.out.println("Hello this is a hack");
+
+
+
+
+
+        int nr1 =  32;
+        int nr2 = 55;
+
+
+
+        String myName = "This is my name";
+
+
+        System.out.println(nr1 + nr2 + myName );        
+
+        System.out.println(myName);
+
+
+    }
+}
